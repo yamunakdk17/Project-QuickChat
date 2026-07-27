848d94cdfdcc3ff55c53255812c94b8e9ba4892c
class-demo <yamunakdk7@gmail.com>
2026-07-26 22:25:43 -0400

    Complete QuickChat MERN application

---
+++ server/middleware/auth.js
@@ -0,0 +1,41 @@
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+// Middleware to protect routes
+export const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.headers.token;
+
+        // Check if token exists
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message: "No token provided",
+            });
+        }
+
+        // Verify token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        // Find user
+        const user = await User.findById(decoded.userId).select("-password");
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found",
+            });
+        }
+
+        req.user = user;
+        next();
+
+    } catch (error) {
+        console.log(error.message);
+
+        return res.status(401).json({
+            success: false,
+            message: "Invalid or expired token",
+        });
+    }
+};
